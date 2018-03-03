6cc5e8a5154e77d15a8e0b44fae1f3d878fe4b55
jagustinbe <j_agustinbe@outlook.com>
2018-03-03 14:56:43 +0000

    initial commit

---
+++ src/main/java/BenitezJorge/Git_test/App.java
@@ -0,0 +1,13 @@
+package BenitezJorge.Git_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
